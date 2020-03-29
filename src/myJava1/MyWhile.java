cba0c8eeb420fd4a3e9a7a57ef560e307095699b
Mohammed Farhan <Mohammed.farhan75@yahoo.de>
2020-03-29 13:51:51 +0200

    using while loop

---
+++ src/myJava1/MyWhile.java
@@ -0,0 +1,20 @@
+package myJava1;
+
+public class MyWhile {
+	
+	public static void main(String [] args) {
+		
+//		wir benutzen hier die einfache While Schleife.
+		
+		int counter=100;
+		
+		while(counter >= 40) {
+			System.out.printf("the next one is : %s%n",counter);
+			counter-=10;
+			
+		}
+		
+		
+	}
+
+}
